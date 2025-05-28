2baff10ac4d68d0399a650f800fc4bc4f8c96171
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-05-28 18:17:31 -0400

    Made a file and a module string that describes the program.

---
+++ Lab1_awilliams#3.py
@@ -0,0 +1,11 @@
+"""Name: Ambrea Williams
+   
+   Date: 05/28/2025
+   
+   Unit 1: Lab 1
+   
+   Description: This is a program that receives a message from the user, 
+   prints the message in reverse, 
+   and shows how many charaters were used in the message.
+"""
+
