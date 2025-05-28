70307f20c1c457bca3c9f84585ffe2b8a2990335
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-05-28 18:41:26 -0400

    Added an variable with a input value for the user to put in a message. A reverse message and a format for the number of characters used in the message.

--- Lab1_awilliams#3.py
+++ Lab1_awilliams#3.py
@@ -4,8 +4,17 @@
    
    Unit 1: Lab 1
    
-   Description: This is a program that receives a message from the user, 
-   prints the message in reverse, 
+   Description: This code receives a message from the user, prints the message in reverse, 
    and shows how many charaters were used in the message.
 """
+
+#Receives the message from the user
+chat_message =input("Enter message into the chat:")
+
+
+#Output the message in reverse
+print("Your message in reverse is:", chat_message[::-1])
+
+#says the number of characters in the message
+print(f"Number of characters: {len(chat_message)}")
 
